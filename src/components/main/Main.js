02379c17dc1160ac60b484ac4647b2ed25762c7f
rum-n <manev.ru@gmail.com>
2020-02-27 15:30:14 +0200

    adding spring

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {useSpring, animated} from 'react-spring';
 import './main.css';
 
 const Main = () => {
-    return(
-        <div>
+    const fade = useSpring({opacity: 1, from: {opacity: 0}, delay: 400})
+    return(        
+        <animated.div style={fade}>
             <h2 className='heading'>Creating stuff for the web, based on <span className="data">data</span>, personal <span className="interests">interests</span> and occasionally ⁠— for other <span className='people'>people</span>.</h2>
-        </div>
-    );
-}
+        </animated.div>    
+    )
+};
 
 export default Main;
