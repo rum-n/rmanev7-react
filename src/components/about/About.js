02c8e3a1c45c59c7af00ab73fdf23adff80b9bdf
rum-n <manev.ru@gmail.com>
2020-02-28 17:05:02 +0200

    responsive + projects page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useSpring, animated} from 'react-spring';
+import Projects from '../projects/Projects';
 import './about.css'
 
 const About = () => {
@@ -8,9 +9,9 @@ const About = () => {
         <animated.div style={fade} className='about'>
             <h1 className='header'>About</h1>
             <p>Most of the time I'm working as a business developer and partnerships manager at <span className='people'><a href="www.melewi.net">Melewi</a></span> - a travelling UX & UI design studio.</p>
-            <p>The rest of time I'm either running, climbing, skiing or working on a personal <span className='data'><a href="#">side project</a></span>.</p>
+            <p>The rest of time I'm either running, climbing, skiing or working on a personal side project.</p>
             <p>I might also be up in the mountains or down by the sea, depending on the season. The point is I'm trying to stay active.</p>
-            <p>In the past few years, I've been semi-actively getting into web technologies, data science, writing, e-commerce and everything else I find interesting. If you have something you want to share or just want to say hi, feel free to <span className='interests'>drop a line.</span></p> 
+            <p>In the past few years, I've been semi-actively getting into web technologies, data science, <span className='data'><a href="www.medium.com/@room_n">writing</a></span>, e-commerce and everything else I find interesting. If you have something you want to share or just want to say hi, feel free to <span className='interests'>drop a line.</span></p> 
             <p>In the meantime - here's a <span className='listen'><a href="https://open.spotify.com/user/room_n/playlist/5bpb7U0wi2OCYkVVl5xY8G?si=u3qK0TOHQ525aOxZffCuOw">playlist</a></span> you can listen to while you work.</p>
         </animated.div>
         )
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,9 +7,42 @@ const Projects = () => {
     return(
         <animated.div style={fade} className='projects'>
             <h1 className='header'>Projects</h1>
-            <p>Coming soon!</p>
+
+            <a href='https://rum-n.github.io/ski/'><div className='projectCard'>
+                <p className='projectName data'>Ski Dashboard</p>
+                <p className='projectBlurb'>An <span className='people'>online dashboard</span> showing ski conditions at several places I like to go ski in Bulgaria. Data is pulled from the Dark Sky API. <br /> <br /> <span className='interests'>Tech Stack:</span> HTML/CSS/JS</p>
+            </div></a>
+
+            <a href='https://codapayments.com'><div className='projectCard'>
+                <p className='projectName data'>Coda Payments website</p>
+                <p className='projectBlurb'>A <span className='people'>website redesign</span> for Coda Payments - one of the leading payment providers for web and mobile gaming in Asia. It's worth mentioning I didn't create the design - only the implementation. <br /> <br /> <span className='interests'>Tech Stack:</span> Webflow</p>
+            </div></a>
+            
+            <a href='https://gripindoor.netlify.com/'><div className='projectCard'>
+                <p className='projectName data'>Gripindoor</p>
+                <p className='projectBlurb'>An <span className='people'>interactive map</span> showing indoor climbing gyms across Europe. At first I was using Middleman as a static site generator, but then rewrote the app and made it simpler. <br /> <br /> <span className='interests'>Tech Stack:</span> HTML/CSS/JS, Leaflet.js</p>
+            </div></a>
+
+            <a href='https://results.netlify.com/'>
+            <div className='projectCard'>
+                <p className='projectName data'>Race Results</p>
+                <p className='projectBlurb'>Visualizing race results from various running events. I wanted to play around with <span className='people'>data visualizations</span> using Highcharts, so I used it for something I like - running.<br /> <br /> <span className='interests'>Tech Stack:</span> HTML/CSS/JS, Highchart.js, R</p>
+            </div></a>
+
+            <a href='https://rum-n.github.io/thewordfuck/'>
+            <div className='projectCard'>
+                <p className='projectName data'>The Word Fuck</p>
+                <p className='projectBlurb'>Again, wanted to test out <span className='people'>data visualizing</span>, this time with D3. I was looking for available data to visualize, which I realize might've been something more informative, but you know how you sometimes randomly stumble across a weird Wikipedia article. Note: it won't show the data if you're using pop-up blockers.<br /> <br /> <span className='interests'>Tech Stack:</span> HTML/CSS/JS, D3.js, R</p>
+            </div></a>
+
+            <div className='projectCard'>
+                <p className='projectName data'>Easter European Art</p>
+                <p className='projectBlurb'>An <span className='people'>online gallery</span> to promote Eastern European artists. My dad paints, so it started as a website showcasing his works, but then I thought others might be interested to feature their work too.<br /> <br /> <span className='interests'>Tech Stack:</span> No-code solution.</p>
+            </div>
         </animated.div>
     );
 }
+
+
 
 export default Projects;
