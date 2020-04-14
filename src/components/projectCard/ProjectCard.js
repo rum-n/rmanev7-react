87d4106a27cbdec4cb97054be574a35a55eea617
rum-n <manev.ru@gmail.com>
2020-04-14 22:27:59 +0300

    project screenshots

--- src/components/projectCard/ProjectCard.js
+++ src/components/projectCard/ProjectCard.js
@@ -6,8 +6,8 @@ const ProjectCard = (props) => {
     <a href={props.link}><div className='projectCard'>
         <p className='projectName data'>{props.title}</p>
         <p className='projectBlurb'>{props.blurb}</p>
-        <p></p>
-        <p className='projectBlurb people'>{props.techstack}</p>
+        <p className='projectTech people'>{props.techstack}</p>
+        <img className='projectCardImage' alt='Ski Dashboard' src={props.image} />
     </div></a>
     );
 }
